de4d0d5eaf23b67e00e94e3debcf5b16d7f2ff76
SydMatters <nicolas.vega@utp.edu.co>
2024-06-29 10:22:52 -0500

    First

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Title from './components/Title'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Title></Title>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Title.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Title() {
+  return <h1 className="mt-0 text-3xl font-bold underline">Title</h1>;
+}
+
+export default Title;
