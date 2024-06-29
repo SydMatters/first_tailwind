9b1f85004cb4553b9e5d440374336b7b9a3f41cc
SydMatters <nicolas.vega@utp.edu.co>
2024-06-29 11:57:44 -0500

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,10 @@
-import Button from "./components/Button";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <header className=" bg-white px-6 shadow">
-        <div className="flex h-16 items-center justify-between">
-          <Button>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-              />
-            </svg>
-          </Button>
-          <a href="#">logo</a>
-          <div>
-            <Button>theme</Button>
-            <Button>user</Button>
-          </div>
-        </div>
-      </header>
+      <Header></Header>
     </>
   );
 }
--- src/components/Nanvar.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Nanvar() {
-  return <div className="flex justify-between h-16 items-center"></div>;
-}
-
-export default Nanvar;
